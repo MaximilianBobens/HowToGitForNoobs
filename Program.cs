73a9d0874e86004881ac19e7378241e0ba6ad09d
maxim <m.bobens@htlkrems.at>
2024-03-06 22:27:52 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HowToGitPerfectForNoobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How to git perfect?");
+            Console.WriteLine("Click to the the version control in Rider");
+            Console.WriteLine("Select your folder");
+            Console.WriteLine("Open Github and generate a repository");
+            Console.WriteLine("Afterwards right click to Namespace Git => Manage Remotes and add the url given in your repository");
+            Console.WriteLine("Example: https://github.com/MaximilianBobens/HowToGitForNoobs.git (It is the HTTPS link)");
+            Console.WriteLine(" Afterwards right click to Namespace Git => Add");
+        }
+    }
+}
