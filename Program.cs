6c1b7141ff9394c138172ef0081df314b307e8e8
maxim <m.bobens@htlkrems.at>
2024-03-06 22:35:03 +0100

    added comments

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ namespace HowToGitPerfectForNoobs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How to git perfect?");
-            Console.WriteLine("Click to the the version control in Rider");
-            Console.WriteLine("Select your folder");
-            Console.WriteLine("Open Github and generate a repository");
-            Console.WriteLine("Afterwards right click to Namespace Git => Manage Remotes and add the url given in your repository");
-            Console.WriteLine("Example: https://github.com/MaximilianBobens/HowToGitForNoobs.git (It is the HTTPS link)");
-            Console.WriteLine(" Afterwards right click to Namespace Git => Add");
+            Console.WriteLine("Click to the the version control in Rider"); // 1
+            Console.WriteLine("Select your folder"); // 2
+            Console.WriteLine("Open Github and create a repository"); // 3
+            Console.WriteLine("Afterwards right click to Namespace Git => Manage Remotes and add the url given in your repository (HTTPS)"); // 4
+            Console.WriteLine("Right click to Namespace Git => Add"); // 5
+            Console.WriteLine("Finally open the commit tab in Rider and select all changes"); // 6
+            Console.WriteLine("Enter commit message and select push and commit"); // 7
+            Console.WriteLine("Check your Git repository :) "); // 8
         }
     }
 }
